78c333730faf8c4d99623cb0c76514f73ac5c08e
Erika Sandoval <erikasandoval-09@hotmail.com>
2021-09-26 01:29:24 -0300

    2do desafio

---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+
+
+const Nav = () => {
+    return(
+    <>
+        
+            <nav className="nav">
+                <ul>
+                    <a href="#">Inicio</a>
+                    <a href="#">Productos</a>
+                    <a href="#">Contacto</a>
+                </ul>
+            </nav>
+        
+    </>
+    )
+}
+
+export default Nav
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//1) Me traigo la librería React
+
+import React from 'react';  
+
+//2) Me traigo la librería ReactDOM
+
+import ReactDOM from 'react-dom'; 
+
+//3) Importo un componente 
+
+import Nav from "./Nav";
+
+import "./estilos.css"
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+//4) Renderizo el componente
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Nav />,
+    document.getElementById("root"))
