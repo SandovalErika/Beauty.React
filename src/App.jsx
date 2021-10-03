29f036fee3f1272839d80e9daca8c7954ded98fd
Erika Sandoval <erikasandoval-09@hotmail.com>
2021-10-02 23:42:05 -0300

    Desafio 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
 
+import { useState } from 'react';
 import Header from './Header';
-//import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container';
 
 const App = () => {
-    return(
-    <>
-        <Header  />
-        {/* <Container>
-            <Main />
-            <Footer />
-        </Container> */}
 
-        
-    </>
+    const [contador, setContador] = useState(0)
+
+    // useEffect(() => {
+
+
+    // })
+
+    //const resultado = useState(0)
+    //let [contador, setContador] = resultado 
+
+    // let  contador = 0
+    //let contador = resultado[0]
+    //let setContador = resultado[1]
+
+    // console.log(resultado);
+
+    const aumentar = () => {
+        // contador = contador + 1; 
+        setContador(contador + 1)
+    }
+
+    return (
+        <>
+            <Header />
+            <Container fluid>
+                <p>El contador va: {contador} </p>
+                <button onClick={aumentar} >Sumar</button>
+                {/* <Main />
+            <Footer /> */}
+            </Container>
+
+
+        </>
     )
 }
 
